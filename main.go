18ef9ccbc1192fecc394219dc24dc23fd6fb98f5
Ammar Ansari <ammar.ice98@gmail.com>
2026-07-11 22:03:57 +0530

    feat: add time-based greeting support

--- main.go
+++ main.go
@@ -3,11 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 )
+
+func greeting() string {
+	hour := time.Now().Hour()
+
+	switch {
+	case hour < 12:
+		return "Good Morning"
+	case hour < 18:
+		return "Good Afternoon"
+	default:
+		return "Good Evening"
+	}
+}
 
 func main() {
 	name := flag.String("name", "World", "Name to greet")
 	flag.Parse()
 
-	fmt.Printf("Hello %s!\n", *name)
+	fmt.Printf("%s, %s!\n", greeting(), *name)
 }
